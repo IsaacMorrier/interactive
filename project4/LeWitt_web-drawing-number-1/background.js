6198b70c9c47d8bcd93500ffb7b830d82c9febe6
IsaacMorrier <isaacmorrier@gmail.com>
2017-05-15 16:38:24 -0400

    background js added and zip updated

---
+++ project4/LeWitt_web-drawing-number-1/background.js
@@ -0,0 +1,17 @@
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+//  // No tabs or host permissions needed!
+//  console.log('Turning ' + tab.url + ' red!');
+////  chrome.tabs.executeScript({
+////    code: 'document.body.style.backgroundColor="red"'
+////  });
+  
+//  chrome.tabs.insertCSS({
+//    file: 'main.css'
+//  });
+  
+	// for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'main.js'
+	});
+});
